9ca1e0a0615a6147e938b0f1a6c164f16f2cb578
Cryptonox1816 <kartikeygupta1122@outlook.com>
2026-02-21 23:15:37 +0530

    day21

---
+++ Day21.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to traverse and print the linked list
+void traverseList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, i, value;
+    struct Node *head = NULL, *tail = NULL;
+
+    // Input size of linked list
+    scanf("%d", &n);
+
+    // Input elements and create linked list
+    for (i = 0; i < n; i++) {
+        scanf("%d", &value);
+        struct Node* newNode = createNode(value);
+
+        if (head == NULL) {
+            head = newNode;  // First node
+            tail = newNode;
+        } else {
+            tail->next = newNode;  // Link new node
+            tail = newNode;        // Move tail
+        }
+    }
+
+    // Traverse and print linked list
+    traverseList(head);
+
+    return 0;
+}
