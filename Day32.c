5e39967c88efe3997aed312158530c88c80952ef
Cryptonox1816 <kartikeygupta1122@outlook.com>
2026-03-04 22:51:42 +0530

    day32

---
+++ Day32.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+// Function to push an element onto the stack
+void push(int x) {
+    if (top == MAX - 1) {
+        printf("Stack Overflow\n");
+    } else {
+        stack[++top] = x;
+    }
+}
+
+// Function to pop an element from the stack
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+    } else {
+        top--;
+    }
+}
+
+// Function to display stack elements from top to bottom
+void display() {
+    for (int i = top; i >= 0; i--) {
+        printf("%d ", stack[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n, m, x;
+
+    // Input number of elements to push
+    scanf("%d", &n);
+
+    // Push n elements
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        push(x);
+    }
+
+    // Input number of pops
+    scanf("%d", &m);
+
+    // Perform m pops
+    for (int i = 0; i < m; i++) {
+        pop();
+    }
+
+    // Display remaining stack
+    display();
+
+    return 0;
+}
