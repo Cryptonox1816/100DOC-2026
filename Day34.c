bec826e8aa600b59b7827f72373be8c5ba3e0876
Cryptonox1816 <kartikeygupta1122@outlook.com>
2026-03-06 18:44:19 +0530

    day34

---
+++ Day34.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+// Node structure for stack
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+// Push operation
+void push(Node** top, int value) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (!newNode) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    newNode->data = value;
+    newNode->next = *top;
+    *top = newNode;
+}
+
+// Pop operation
+int pop(Node** top) {
+    if (*top == NULL) {
+        printf("Stack underflow\n");
+        exit(1);
+    }
+    Node* temp = *top;
+    int value = temp->data;
+    *top = temp->next;
+    free(temp);
+    return value;
+}
+
+// Evaluate postfix expression
+int evaluatePostfix(char* expr) {
+    Node* stack = NULL;
+    char* token = strtok(expr, " ");
+
+    while (token != NULL) {
+        if (isdigit(token[0])) {
+            // Operand → push to stack
+            push(&stack, atoi(token));
+        } else {
+            // Operator → pop two operands
+            int val2 = pop(&stack);
+            int val1 = pop(&stack);
+            int result;
+
+            switch (token[0]) {
+                case '+': result = val1 + val2; break;
+                case '-': result = val1 - val2; break;
+                case '*': result = val1 * val2; break;
+                case '/': result = val1 / val2; break;
+                default:
+                    printf("Invalid operator: %s\n", token);
+                    exit(1);
+            }
+            push(&stack, result);
+        }
+        token = strtok(NULL, " ");
+    }
+
+    return pop(&stack);
+}
+
+int main() {
+    char expr[100];
+    printf("Enter postfix expression: ");
+    fgets(expr, sizeof(expr), stdin);
+
+    // Remove newline if present
+    expr[strcspn(expr, "\n")] = '\0';
+
+    int result = evaluatePostfix(expr);
+    printf("Result: %d\n", result);
+
+    return 0;
+}
