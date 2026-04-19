00378992d63b0064efee67e16b22dec25d3396dc
Cryptonox1816 <kartikeygupta1122@outlook.com>
2026-04-19 23:15:01 +0530

    day78

---
+++ Day78.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define MAX 1000
+
+int minKey(int key[], int mstSet[], int n) {
+    int min = INT_MAX, min_index;
+    for (int v = 0; v < n; v++) {
+        if (mstSet[v] == 0 && key[v] < min) {
+            min = key[v];
+            min_index = v;
+        }
+    }
+    return min_index;
+}
+
+int primMST(int graph[MAX][MAX], int n) {
+    int parent[n];   // Stores MST
+    int key[n];      // Key values to pick minimum weight edge
+    int mstSet[n];   // To represent set of vertices included in MST
+
+    for (int i = 0; i < n; i++) {
+        key[i] = INT_MAX;
+        mstSet[i] = 0;
+    }
+
+    key[0] = 0;      // Start from first vertex
+    parent[0] = -1;  // First node is root of MST
+
+    for (int count = 0; count < n - 1; count++) {
+        int u = minKey(key, mstSet, n);
+        mstSet[u] = 1;
+
+        for (int v = 0; v < n; v++) {
+            if (graph[u][v] && mstSet[v] == 0 && graph[u][v] < key[v]) {
+                parent[v] = u;
+                key[v] = graph[u][v];
+            }
+        }
+    }
+
+    int totalWeight = 0;
+    for (int i = 1; i < n; i++) {
+        totalWeight += graph[i][parent[i]];
+    }
+    return totalWeight;
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int graph[MAX][MAX] = {0};
+
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        scanf("%d %d %d", &u, &v, &w);
+        // Convert to 0-based indexing
+        u--; v--;
+        graph[u][v] = w;
+        graph[v][u] = w;
+    }
+
+    int result = primMST(graph, n);
+    printf("%d\n", result);
+
+    return 0;
+}
