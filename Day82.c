0686883224440e352fa7507e78d9f6fd1c575684
Cryptonox1816 <kartikeygupta1122@outlook.com>
2026-04-23 22:21:46 +0530

    day82

---
+++ Day82.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+// Function to find Lower Bound
+int lowerBound(int arr[], int n, int x) {
+    int low = 0, high = n;
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] < x)
+            low = mid + 1;
+        else
+            high = mid;
+    }
+    return low; // index of first element >= x
+}
+
+// Function to find Upper Bound
+int upperBound(int arr[], int n, int x) {
+    int low = 0, high = n;
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] <= x)
+            low = mid + 1;
+        else
+            high = mid;
+    }
+    return low; // index of first element > x
+}
+
+int main() {
+    int n, x;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    scanf("%d", &x);
+
+    int lb = lowerBound(arr, n, x);
+    int ub = upperBound(arr, n, x);
+
+    printf("%d %d\n", lb, ub);
+    return 0;
+}
